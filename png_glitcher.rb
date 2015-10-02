c62c4c4624e007aa49b3582b9c377d769663984c
Allen Garvey <allengarvey@hotmail.com>
2015-10-01 23:26:36 -0400

    add constants for output directory and output file suffix

--- png_glitcher.rb
+++ png_glitcher.rb
@@ -1,7 +1,9 @@
 require "pnglitch"
 
 INPUT_FILE_NAME = 'sample'
-INPUT_DIRECTORY = "#{Dir.home}/Desktop/" #requires Ruby 1.9.3 or later for Dir.home
+INPUT_DIRECTORY = "#{Dir.home}/Desktop/"
+OUPUT_DIRECTORY = INPUT_DIRECTORY
+OUTPUT_FILE_SUFFIX = '-glitched'
 
 FILTERS = {none: 0, sub: 1,  up: 2, average: 3, paeth: 4}
 
@@ -12,5 +14,5 @@ PNGlitch.open("#{INPUT_DIRECTORY}#{INPUT_FILE_NAME}.png") do |png|
   png.glitch do |data|
     data.gsub /\d/, 'x'
   end
-  png.save "#{INPUT_DIRECTORY}#{INPUT_FILE_NAME}-glitched.png"
+  png.save "#{OUTPUT_DIRECTORY}#{INPUT_FILE_NAME}#{OUTPUT_FILE_SUFFIX}.png"
 end
